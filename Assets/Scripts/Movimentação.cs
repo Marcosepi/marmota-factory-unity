9d107eed167d38c38158e3d781fda643efb4d043
sergiofffilho <sergiofffilho@gmail.com>
2014-11-28 19:48:36 -0200

    Script de velocidade dos personagens

---
+++ Assets/Scripts/Movimentação.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimentação : MonoBehaviour {
+
+	public float velocidade;
+	
+	void Start () {
+		velocidade = 0.001f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate ();
+		transform.position = new Vector3(transform.position.x+velocidade, transform.position.y, transform.position.z);
+	}
+
+	void OnTriggerStay2D(Collider2D c){
+		if (c.CompareTag (gameObject.tag)) {
+			velocidade = 0.005f;
+		}else{
+			velocidade = 0.003f;
+		}
+	}
+
+}
