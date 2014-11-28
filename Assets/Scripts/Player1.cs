edefe6dcb5094d8cad62c872ead847c4d4ff4054
Neils01 <iurryy@yahoo.com.br>
2014-11-28 20:04:50 -0300

    movimentação de perssonagem

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1 : MonoBehaviour {
+	
+	public int Speed;
+	public bool pulando;
+	public GameObject soco;
+	
+	// Use this for initialization
+	void Start () {
+		Speed = 5;
+		pulando = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector2.right * Speed * Time.deltaTime);
+	}
+	
+	/*void jump() {
+		if (!pulando) {
+			rigidbody2D.AddForce (Vector2.up * Speed * 40);
+			pulando = true;
+		}
+	}*/
+	
+	void atk() {
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+		if(collision.gameObject.tag == "terreno") {
+			pulando=false;
+		}
+	}
+}
