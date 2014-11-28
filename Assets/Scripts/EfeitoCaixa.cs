b0c5d57a721bc71e555d675bc8ce89edbf44e364
AmaroCesar <amro_txt@homail.com>
2014-11-28 21:20:08 -0200

    Escripts para acao da caixa sobre usuarios e inicio script manager

---
+++ Assets/Scripts/EfeitoCaixa.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EfeitoCaixa : MonoBehaviour {
+	private string minhaTag;
+	private string tagCor;
+
+	private bool utilizarCaixa;
+	private Manager _manager ;
+	void Start(){
+		minhaTag = Component.tag;
+		utilizarCaixa = false;
+		_manager = (Manager) GameObject.FindWithTag("GamaManager").GetComponent("Manager");
+}
+
+	void FixedUpdate(){
+		if (utilizarCaixa){
+			int posicao;
+			if (Input.GetKey("1")){
+				posicao =1;
+				_manager.EnviaEfeitoCaixa(posissao,tagCor);
+			}
+			if (Input.GetKey("2")){
+				posicao =2;
+				_manager.EnviaEfeitoCaixa(posissao,tagCor);
+
+			}
+			if (Input.GetKey("3")){
+				posicao =3;
+				_manager.EnviaEfeitoCaixa(posissao,tagCor);
+
+			}
+			if (Input.GetKey("4")){
+				posicao =4;
+				_manager.EnviaEfeitoCaixa(posissao,tagCor);
+			}
+
+		
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D c){
+		utilizarCaixa = true;
+		tagCor = c.collider.tag;	
+
+//		if(c.collider.CompareTag(minhaTag)){
+//		}
+//		else{
+//			rigidbody.AddForce (Vector3.right*2);
+//		}
+	}
+
+
+}
